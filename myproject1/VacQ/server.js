d8daff96230c22825f635635349383fb7a80a278
My Name <6733171021@student.chula.ac.th>
2025-02-19 10:33:16 +0700

    Initial Express setup

---
+++ myproject1/VacQ/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config({path:'./config/config.env'});
+const app=express();
+app.get('/', (req,res)=>{
+    res.status(200).json({success:true, data:{id:1}});
+});
+
+const PORT=process.env.PORT || 5000;
+app.listen(PORT,console.log('server running in ',process.env.NODE_ENV,' mode on port ', PORT));
