cdcca58438c013642e36afa5d401c06c9fb0e459
Sarisa <6733268221@student.chula.ac.th>
2025-02-28 21:16:43 +0700

    logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -76,3 +76,12 @@ exports.getMe = async (req, res, next) => {
 
     res.status(200).json({success: true, data: user});
 };
+
+
+exports.logout =async(req,res,next)=>{
+    res.cookie('token','none',{
+        expires: new Date(Date.now()+ 10*1000),httpOnly:true
+    });
+
+    res.status(200).json({success:true,data:{}});
+};
